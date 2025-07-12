a04497dfa7875604c4dccf0b55a71b4bc985d6ef
CamelleoDev <camelleodev@gmail.com>
2025-07-11 21:58:48 -0400

    a

--- VulgarWordGenerator.py
+++ VulgarWordGenerator.py
@@ -1,17 +1,17 @@
 from itertools import product
 
 leet_map = {
-    'A': ['A', '4'],
-    'E': ['E', '3'],
-    'I': ['I', '1'],
-    'L': ['L', '1'],
-    'O': ['O', '0'],
-    'S': ['S', '5'],
-    'T': ['T', '7'],
-    'G': ['G', '6']
+    'A': ['A', '4'], 'a': ['a', '4'],
+    'E': ['E', '3'], 'e': ['e', '3'],
+    'I': ['I', '1'], 'i': ['i', '1'],
+    'L': ['L', '1'], 'l': ['l', '1'],
+    'O': ['O', '0'], 'o': ['o', '0'],
+    'S': ['S', '5'], 's': ['s', '5'],
+    'T': ['T', '7'], 't': ['t', '7'],
+    'G': ['G', '6'], 'g': ['g', '6']
 }
 
-input_text = input("Enter text to generate leetspeak combinations: ").upper()
+input_text = input("Enter text to generate leetspeak combinations: ")
 
 options = []
 for char in input_text:
